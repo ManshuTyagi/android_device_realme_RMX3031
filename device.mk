3b02e1b603aa313854cd8c764a5a701d36cbafb5
Maanush Putcha <p.maanush@gmail.com>
2021-09-21 22:18:53 +0530

    RMX3031: Add Custom Light Hal
    Signed-off-by: Maanush Putcha <p.maanush@gmail.com>
    Signed-off-by: ManshuTyagi <himanshut567@gmail.com>
    Change-Id: I647cfadb187cb9f4e09e825a3ba59af653491166

--- device.mk
+++ device.mk
@@ -58,6 +58,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_AAPT_CONFIG := xxxhdpi
 PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.RMX3031
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH)
