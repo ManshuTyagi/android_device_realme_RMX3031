bb9d7d1ced3afbd14d483f70eb58d4bf34739081
LuK1337 <priv.luk@gmail.com>
2021-10-10 18:03:51 +0530

    RMX3031: Add vintf fragments for FOD and Fingerprint HAL
    Change-Id: Id36d5f4389a2354d1128bd6628617a4a5e4febba
    Signed-off-by: ManshuTyagi <himanshut567@gmail.com>

--- fingerprint/BiometricsFingerprint.cpp
+++ fingerprint/BiometricsFingerprint.cpp
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#define LOG_TAG "android.hardware.biometrics.fingerprint@2.1-service.realme_RMX3031"
+#define LOG_TAG "android.hardware.biometrics.fingerprint@2.1-service.RMX3031"
 #define LOG_VERBOSE "android.hardware.biometrics.fingerprint@2.1-service.realme_RMX3031"
 
 #include <hardware/hardware.h>
--- fingerprint/service.cpp
+++ fingerprint/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.biometrics.fingerprint@2.1-service.realme_RMX3031"
+#define LOG_TAG "android.hardware.biometrics.fingerprint@2.1-service.RMX3031"
 
 #include <android-base/logging.h>
 #include <hidl/HidlTransportSupport.h>
