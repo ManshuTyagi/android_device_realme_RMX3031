3abd55f5dc61cb4ecf69978f5870c0904c410257
ManshuTyagi <himanshut567@gmail.com>
2021-10-21 03:56:34 +0530

    RMX3031: Set PRODUCT_EXTRA_VNDK_VERSIONS to 30
    Change-Id: If622aca50e75e9978f0a75a696c0b226b17e831a
    Signed-off-by: ManshuTyagi <himanshut567@gmail.com>

--- device.mk
+++ device.mk
@@ -41,12 +41,15 @@ TARGET_SCREEN_WIDTH := 1080
 # A/B
 AB_OTA_UPDATER := false
 
+# VNDK
+PRODUCT_EXTRA_VNDK_VERSIONS := 30
+
 # Audio
 PRODUCT_PACKAGES += \
     audio.a2dp.default
 
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PLATFORM_VNDK_VERSION)/etc/audio_policy_configuration.xml
+    $(DEVICE_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_EXTRA_VNDK_VERSIONS)/etc/audio_policy_configuration.xml
 
 # fastbootd
 PRODUCT_PACKAGES += \
