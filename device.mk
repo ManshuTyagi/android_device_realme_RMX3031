213bc28123b91df2353e0a4b2a710e4536a2deca
ManshuTyagi <himanshut567@gmail.com>
2021-10-27 19:04:28 +0530

    RMX3031: Compile Realme parts if exists
    Signed-off-by: ManshuTyagi <himanshut567@gmail.com>
    Change-Id: I4a45883338bc7dc5403e201008e5008752250126

--- device.mk
+++ device.mk
@@ -22,6 +22,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Parts
+$(call inherit-product-if-exists, packages/apps/RealmeParts/parts.mk)
+
 PRODUCT_SHIPPING_API_LEVEL := 30
 
 # Call proprietary blob setup
