332e3f29dea9186e32bc7d3e0c631fe849bfc16e
XsaviXander <22182013+XsaviXander@users.noreply.github.com>
2022-04-21 23:59:39 +0530

    RMX3031:  Enable freeform window management
    Change-Id: Iede2898eb8e1efcdc3b95c3d89477eca8dd31609
    Signed-off-by: ManshuTyagi <himanshut567@gmail.com>

--- device.mk
+++ device.mk
@@ -62,6 +62,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.fingerprint.xml
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # HIDL
 PRODUCT_PACKAGES += \
     libhidltransport \
