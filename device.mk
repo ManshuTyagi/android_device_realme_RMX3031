99a5fadbfe562cd62d92723d35cd82e7da0db616
ManshuTyagi <himanshut567@gmail.com>
2022-04-21 19:28:11 +0530

    RMX3031: Import libnfc-nxp.conf
    Signed-off-by: ManshuTyagi <himanshut567@gmail.com>
    Change-Id: Id62fc431eaa81095ce3f00daca38f7c246dc3b48

--- device.mk
+++ device.mk
@@ -82,6 +82,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/keylayout/touchpanel.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/touchpanel.kl
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nxp.conf
+
 # Screen density
 PRODUCT_AAPT_CONFIG := xxxhdpi
 PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
