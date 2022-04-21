ed3cb86f45400ab6ce32283293c48e1855ca32be
Martin Liu <liumartin@google.com>
2022-04-21 23:50:00 +0530

    RMX3031: prop: Adjust zram write back policy
    Change-Id: I32e4e99fa8397ffaa2731fd0bc46d8afc06b0bf3
    Signed-off-by: ManshuTyagi <himanshut567@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -52,7 +52,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 # ZRAM
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.zram.mark_idle_delay_mins=60 \
-    ro.zram.first_wb_delay_mins=180 \
+    ro.zram.first_wb_delay_mins=1440 \
     ro.zram.periodic_wb_delay_hours=24
 
 # Encryption
